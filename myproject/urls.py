a5263e74f498c372cb4143b4c43aa56ebd98e27a
Valdis <v.beikmanis@gmail.com>
2024-07-28 23:24:31 +0100

    Add root URL pattern

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from newapp import views  # Make sure to import the views from your app
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('ai/', include('newapp.urls')),  # Add this line
+    path('', views.index, name='index'),  # Add this line to include the root URL
+    path('ai/', include('newapp.urls')),  # Include the URLs from your app
 ]
